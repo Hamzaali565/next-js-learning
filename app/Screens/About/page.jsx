e47380b4ab9d06da70af8ca53fa96d803097cc38
Hamzaali565 <ha6153806@gmail.com>
2024-10-10 12:42:04 +0500

    Component implimentation

--- app/Screens/About/page.jsx
+++ app/Screens/About/page.jsx
@@ -1,7 +1,14 @@
+import Input from "@/app/Components/Input";
 import React from "react";
 
 const About = () => {
-  return <div>hello i'm about</div>;
+  return (
+    <div>
+      <p>hello i'm about</p>
+      <p>hello i'm about</p>
+      <Input />
+    </div>
+  );
 };
 
 export default About;
